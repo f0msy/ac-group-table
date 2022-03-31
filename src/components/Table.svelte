6ab0e5d1201a73477c86abca8aa5264d1e85f38c
f0msy <fomsy29@gmail.com>
2022-03-31 16:23:39 +0300

    wip

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -65,8 +65,7 @@
 
         {:catch error}
         <div class="ac-table-error-block">
-
-            <div class="ac-table-name">Ошибка загрузки данных</div>
+            <div class="ac-table-name" style="margin-top: 10px">Ошибка загрузки данных</div>
             <div class="ac-table-error-message"> 
                 <p style="color: red">{error.message}</p>
             </div>
@@ -86,7 +85,7 @@
         font-size: 20px;
         display: flex;
         align-items: center;
-        justify-content: center;
+        justify-content: flex-start;
         margin: 0 10px;
     }
 
